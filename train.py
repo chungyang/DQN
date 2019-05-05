ba85fd318ec252db81ada8249c86dd822b82d42e
chung <chung.yang@engineer.uconn.edu>
2019-05-05 16:14:53 -0400

    add configurable training parameters

--- train.py
+++ train.py
@@ -1,19 +1,23 @@
 import torch
+import argparse
 import gym
 import matplotlib.pyplot as plt
 from utils import *
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-batch_size',type=int, default=128)
+    parser.add_argument('-gamma', type=float, default=0.999)
+    parser.add_argument('-eps_start', type=float, default=0.9)
+    parser.add_argument('-eps_end', type=float, default=0.05)
+    parser.add_argument('-eps_decay', type=int, default=200)
+    parser.add_argument('-target_update', type=int, default=10)
+    parser.add_argument('-env', type=str, default='CartPole-v0')
+
+    opt = parser.parse_args()
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    env = gym.make('CartPole-v0').unwrapped
-
-
-    env.reset()
-    plt.figure()
-    plt.imshow(get_screen(env,device).squeeze(0).permute(1, 2, 0).numpy(),
-               interpolation='none')
-    plt.title('Example extracted screen')
-    plt.show()
+    env = gym.make(opt.env).unwrapped
 
 
