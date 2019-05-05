b206a163db2fb11fcb63b4e4a29af824c328b923
chung <chung.yang@engineer.uconn.edu>
2019-05-05 15:33:18 -0400

    minor fix for residual connection

--- DQN.py
+++ DQN.py
@@ -3,8 +3,9 @@ from torch.functional import F
 
 class DQN(nn.Module):
 
-    def __init__(self, h, w, outputs):
+    def __init__(self, h, w, outputs, res_connect):
         super(DQN, self).__init__()
+        self.res_connect = res_connect
         self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
         self.bn1 = nn.BatchNorm2d(16)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
@@ -27,6 +28,9 @@ class DQN(nn.Module):
         res_connection = x
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
-        x += res_connection
+
+        if res_connection:
+            x += res_connection
+            
         x = F.relu(self.bn3(self.conv3(x)))
         return self.head(x.view(x.size(0), -1))
