7003aaa832c74257cd54d456c2721a23ccfdd0da
Hao <hao.cheam@gmail.com>
2019-05-08 18:13:45 -0400

    added PrioritizedReplayBuffer

--- ReplayBuffer.py
+++ ReplayBuffer.py
@@ -7,6 +7,26 @@ class ReplayBuffer(object):
     def __init__(self, capacity):
         self.buffer = deque(maxlen=capacity)
 
+    def push(self, state, action, reward, next_state, done):
+        state = np.expand_dims(state, 0)
+        next_state = np.expand_dims(next_state, 0)
+
+        self.buffer.append((state, action, reward, next_state, done))
+
+    def sample(self, batch_size):
+        state, action, reward, next_state, done = zip(*random.sample(self.buffer, batch_size))
+        return np.concatenate(state), action, reward, np.concatenate(next_state), done
+
+    def __len__(self):
+        return len(self.buffer)
+
+
+
+
+class PrioritizedReplayBuffer(object):
+    def __init__(self, capacity):
+        self.buffer = deque(maxlen=capacity)
+
     def push(self, state, action, reward, next_state, done, weight): # added weight to transition
         state = np.expand_dims(state, 0)
         next_state = np.expand_dims(next_state, 0)
@@ -16,8 +36,10 @@ class ReplayBuffer(object):
     def sample(self, batch_size):
         #state, action, reward, next_state, done = zip(*random.sample(self.buffer, batch_size))
         weights = zip(*self.buffer)[5] #probabilities
-        state, action, reward, next_state, done, weight = np.random.choice(len(self.buffer),batch_size,p=weights) # weighted sampling
+        idx = np.random.choice(len(self.buffer), batch_size, p=probs)
+        state, action, reward, next_state, done, weight= np.random.choice(len(self.buffer),batch_size,p=weights) # weighted sampling
         return np.concatenate(state), action, reward, np.concatenate(next_state), done, weight
 
     def __len__(self):
         return len(self.buffer)
+
