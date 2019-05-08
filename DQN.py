2779eeeaff7f079d92147e69556a5b1d8df734de
chung <chung.yang@engineer.uconn.edu>
2019-05-08 16:56:04 -0400

    modularize DQN for cartpole

--- DQN.py
+++ DQN.py
@@ -1,52 +1,12 @@
 import math, random
-
 import gym
-import numpy as np
-
-import torch
-import torch.nn as nn
 import torch.optim as optim
-import torch.autograd as autograd
-import torch.nn.functional as F
+import argparse
 
-from IPython.display import clear_output
-import matplotlib.pyplot as plt
+from torch import nn
+from ReplayBuffer import ReplayBuffer
+from utils import *
 
-USE_CUDA = torch.cuda.is_available()
-Variable = lambda *args, **kwargs: autograd.Variable(*args, **kwargs).cuda() if USE_CUDA else autograd.Variable(*args, **kwargs)
-
-from collections import deque
-
-
-class ReplayBuffer(object):
-    def __init__(self, capacity):
-        self.buffer = deque(maxlen=capacity)
-
-    def push(self, state, action, reward, next_state, done):
-        state = np.expand_dims(state, 0)
-        next_state = np.expand_dims(next_state, 0)
-
-        self.buffer.append((state, action, reward, next_state, done))
-
-    def sample(self, batch_size):
-        state, action, reward, next_state, done = zip(*random.sample(self.buffer, batch_size))
-        return np.concatenate(state), action, reward, np.concatenate(next_state), done
-
-    def __len__(self):
-        return len(self.buffer)
-
-
-env_id = "CartPole-v0"
-env = gym.make(env_id)
-
-
-epsilon_start = 1.0
-epsilon_final = 0.01
-epsilon_decay = 500
-
-epsilon_by_frame = lambda frame_idx: epsilon_final + (epsilon_start - epsilon_final) * math.exp(-1. * frame_idx / epsilon_decay)
-
-plt.plot([epsilon_by_frame(i) for i in range(10000)])
 
 class DQN(nn.Module):
     def __init__(self, num_inputs, num_actions):
@@ -73,82 +33,65 @@ class DQN(nn.Module):
         return action
 
 
-model = DQN(env.observation_space.shape[0], env.action_space.n)
+def train(env, num_frames, model, gamma, replay_buffer_size, batch_size):
+    losses = []
+    all_rewards = []
+    optimizer = optim.Adam(model.parameters())
+    replay_buffer = ReplayBuffer(replay_buffer_size)
 
-if USE_CUDA:
-    model = model.cuda()
+    state = env.reset()
+    episode_reward = 0
 
-optimizer = optim.Adam(model.parameters())
+    for frame_idx in range(1, num_frames + 1):
+        epsilon = epsilon_by_frame(frame_idx)
+        action = model.act(state, epsilon)
 
-replay_buffer = ReplayBuffer(1000)
+        next_state, reward, done, _ = env.step(action)
+        replay_buffer.push(state, action, reward, next_state, done)
+        state = next_state
+        episode_reward += reward
 
+        if done:
+            print(episode_reward)
+            state = env.reset()
+            all_rewards.append(episode_reward)
+            episode_reward = 0
 
-def compute_td_loss(batch_size):
-    state, action, reward, next_state, done = replay_buffer.sample(batch_size)
+        if len(replay_buffer) > batch_size:
+            loss = compute_td_loss(model, optimizer, batch_size, gamma, replay_buffer)
+            losses.append(loss.item())
 
-    state = Variable(torch.FloatTensor(np.float32(state)))
-    next_state = Variable(torch.FloatTensor(np.float32(next_state)), volatile=True)
-    action = Variable(torch.LongTensor(action))
-    reward = Variable(torch.FloatTensor(reward))
-    done = Variable(torch.FloatTensor(done))
+        if frame_idx % 200 == 0:
+            plot(frame_idx, all_rewards, losses)
 
-    q_values = model(state)
-    next_q_values = model(next_state)
+if __name__ == "__main__":
 
-    q_value = q_values.gather(1, action.unsqueeze(1)).squeeze(1)
-    next_q_value = next_q_values.max(1)[0]
-    expected_q_value = reward + gamma * next_q_value * (1 - done)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-num_frames',  default=10000)
+    parser.add_argument('-replay_buffer_size', default=1000)
+    parser.add_argument('-batch_size', default=32)
+    parser.add_argument('-gamma', default=0.99)
 
-    loss = (q_value - Variable(expected_q_value.data)).pow(2).mean()
+    opt = parser.parse_args()
 
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+    env_id = "CartPole-v0"
+    env = gym.make(env_id)
 
-    return loss
+    epsilon_start = 1.0
+    epsilon_final = 0.01
+    epsilon_decay = 500
+    epsilon_by_frame = lambda frame_idx: epsilon_final + (epsilon_start - epsilon_final) * math.exp(
+        -1. * frame_idx / epsilon_decay)
 
-def plot(frame_idx, rewards, losses):
-    clear_output(True)
-    plt.figure(figsize=(20,5))
-    plt.subplot(131)
-    plt.title('frame %s. reward: %s' % (frame_idx, np.mean(rewards[-10:])))
-    plt.plot(rewards)
-    plt.subplot(132)
-    plt.title('loss')
-    plt.plot(losses)
-    plt.show()
+    model = DQN(env.observation_space.shape[0], env.action_space.n)
 
-
-num_frames = 10000
-batch_size = 32
-gamma = 0.99
-
-losses = []
-all_rewards = []
-episode_reward = 0
-
-state = env.reset()
-for frame_idx in range(1, num_frames + 1):
-    epsilon = epsilon_by_frame(frame_idx)
-    action = model.act(state, epsilon)
-
-    next_state, reward, done, _ = env.step(action)
-    replay_buffer.push(state, action, reward, next_state, done)
+    if USE_CUDA:
+        model = model.cuda()
 
 
+    num_frames = 10000
+    replay_buffer_size = 10000
+    batch_size = 32
+    gamma = 0.99
 
-    state = next_state
-    episode_reward += reward
-
-    if done:
-        print(episode_reward)
-        state = env.reset()
-        all_rewards.append(episode_reward)
-        episode_reward = 0
-
-    if len(replay_buffer) > batch_size:
-        loss = compute_td_loss(batch_size)
-        losses.append(loss.item())
-
-    if frame_idx % 200 == 0:
-        plot(frame_idx, all_rewards, losses)
+    train(env, opt.num_frames, model, opt.gamma, opt.replay_buffer_size, opt.batch_size)
